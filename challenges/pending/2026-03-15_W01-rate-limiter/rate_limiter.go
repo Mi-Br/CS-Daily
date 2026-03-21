c1b5ed916cd8075eac05a60b3305a7e916482804
michail bredichin <michail@bredichin@gmail.com>
2026-03-21 08:52:02 +0100

    fixed mutex lock

--- challenges/pending/2026-03-15_W01-rate-limiter/rate_limiter.go
+++ challenges/pending/2026-03-15_W01-rate-limiter/rate_limiter.go
@@ -30,27 +30,23 @@ func NewRateLimiter(limit int, window time.Duration) *RateLimiter {
 // Allow returns true if the client is within their rate limit, false if exceeded.
 // It must be safe to call concurrently from multiple goroutines.
 func (r *RateLimiter) Allow(clientID string) bool {
+	r.mu.Lock()
+	defer r.mu.Unlock()
 	client, exist := r.clients[clientID]
 	if !exist {
-		r.mu.Lock()
 		r.clients[clientID] = &clientState{
 			count:       1,
 			windowStart: time.Now(),
 		}
-		r.mu.Unlock()
 		return true
 	}
 	if time.Since(client.windowStart) > r.window {
-		r.mu.Lock()
 		client.count = 1
 		client.windowStart = time.Now()
-		r.mu.Unlock()
 		return true
 
 	} else if client.count < r.limit {
-		r.mu.Lock()
 		client.count++
-		r.mu.Unlock()
 		return true
 	}
 	return false
